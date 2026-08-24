da7aed124d1a564d992b54afac54b35b980062be
Nguyen Ba Quoc Long <nbquoclong@gmail.com>
2026-08-24 12:46:43 +0700

    Solve softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -3,16 +3,9 @@ import numpy as np
 def softmax(x: list) -> np.ndarray:
     """Return stable softmax probabilities with the same shape as x."""
     # Write code here
-    x = np.array(x)
+    x = np.array(x, dtype = float)
     if x.ndim == 1:
-        p = np.zeros((1, len(x)))
         x = x - np.max(x)
-        sum = np.sum(np.exp(x))
-        p = np.exp(x)/sum
-    else:
-        p = np.zeros((len(x), len(x[0])))
-        for i in range(len(x)):
-            x[i] = x[i] - np.max(x[i])
-            sum = np.sum(np.exp(x[i]))
-            p[i] = np.exp(x[i])/sum
-    return p
+        return np.exp(x)/np.sum(np.exp(x))
+    x = x - np.max(x, axis = 1, keepdims = True)
+    return np.exp(x)/np.sum(np.exp(x), axis = 1, keepdims = True)
